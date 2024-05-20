990cd6bc34fb5674d455740db79fbd62f021c1c6
N Javier Diaz <njavi.diaz@gmail.com>
2024-05-20 00:44:30 -0300

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,7 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), svelte()]
+  integrations: [tailwind(), react(), svelte()],
+  site: 'https://NJaDiaz.github.io',
+  base: 'https://github.com/NJaDiaz/PagIgle.git',
 });
